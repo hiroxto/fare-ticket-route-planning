4b4062ed1ee80fa0612fac3a49d0364c3eaa8573
Hiroto Kitazawa <hiroxto@gmail.com>
2025-01-03 00:07:05 +0900

    feat: ページのタイトルと説明を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
-"use client";
+import type { Metadata } from "next";
+import Content from "./content";
 
-import Note from "@/components/Route/Note";
-import Output from "@/components/Route/Output";
-import Route from "@/components/Route/Route";
-import Setting from "@/components/Route/Setting";
-import React from "react";
-
-// export const metadata: Metadata = {
-//     title: "乗車券の経路作成",
-//     description: "複雑な経路の乗車券作る際の補助ツール",
-// };
+export const metadata: Metadata = {
+    title: "乗車券の経路作成",
+    description: "複雑な経路の乗車券作る際の補助ツール",
+};
 
 export default function Home() {
-    return (
-        <div className="app">
-            <div className="container">
-                <div className="title-group">
-                    <h1 className="title">乗車券の経路作成</h1>
-                    <p className="description">複雑な経路の乗車券作る際の補助ツール</p>
-                </div>
-
-                <div className="grid grid-cols-12 xl:gap-4">
-                    <Setting></Setting>
-
-                    <Route></Route>
-
-                    <Note></Note>
-
-                    <Output></Output>
-                </div>
-            </div>
-        </div>
-    );
+    return <Content />;
 }
